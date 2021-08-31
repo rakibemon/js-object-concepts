ab58a79c1672847a0cf600b5310cd27ece748c39
rakibemon <rakibemon03@gmail.com>
2021-08-31 18:05:58 +0600

    first commit

---
+++ object-create.js
@@ -0,0 +1,7 @@
+// 1. using object literal
+const student = {names:"EMON", age : 25};
+// 2. Object Constructor
+const person = new Object();
+// 3. 
+const human = Object.create()
+console.log(human);
